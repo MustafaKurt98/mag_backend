83751a772d1deeafbd6222aade43cd7472696ce5
MustafaKurt98 <mustafakurt98@outlook.com>
2023-01-04 14:45:17 +0300

    whole sentences search api comp.

--- controller/product/product_search.js
+++ controller/product/product_search.js
@@ -4,7 +4,7 @@ const searchProductAPI = async (req, res) => {
     const { search } = req.query;
     console.log(search);
     try {
-        const result = await ProductModel.find({ id: search }).exec();
+        const result = await ProductModel.where('id').regex(search).exec();
         res.status(200).json(result);
     } catch (e) {
         res.status(500).json({ message: e.message });
