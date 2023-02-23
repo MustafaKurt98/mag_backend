d72b425f01c9937c1394cabd0ae08e56fd25b167
MustafaKurt98 <mustafakurt98@outlook.com>
2023-02-23 12:25:37 +0300

    partstore xlsx & fs func

--- assan/app.js
+++ assan/app.js
@@ -1,104 +1,368 @@
+// const fs = require('fs');
+// const VCard = require('vcard-creator').default
+
+
+// /* fs.readFile('gokcekmakina.com_users.txt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     const lines = data.split('\n');
+//     lines.forEach(line => {
+//         const [name] = line.split('@')
+//         const [email] = line.split(',');
+//         const myVCard = new VCard()
+//         myVCard
+//             .addName(name+'(gokcekmakina)')
+//             .addEmail(email)
+//         fs.mkdir('gokcek', { recursive: true }, (err) => {
+//             if (err) throw err;
+//         }
+//         )
+//         fs.writeFile(`gokcek/${name}(gokcekmakina).vcf`, myVCard.toString(), function (err) {
+//             if (err) throw err;
+//             console.log('Saved!');
+//         });
+//     }
+//     )
+// }
+// ) */
+// fs.readFile('gokcekgroup.com_users.txt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     const lines = data.split('\n');
+//     lines.forEach(line => {
+//         const [name] = line.split('@')
+//         const [email] = line.split(',');
+//         const myVCard = new VCard()
+//         myVCard
+//             .addName(name+'(gokcekgroup)')
+//             .addEmail(email)
+
+//         fs.writeFile(`gokcek/${name}(gokcekgroup).vcf`, myVCard.toString(), function (err) {
+//             if (err) throw err;
+//             console.log('Saved!');
+//         });
+//     }
+//     )
+// }
+// )
+
+// fs.readFile('magelektronik.com_users.txt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     const lines = data.split('\n');
+//     lines.forEach(line => {
+//         const [name] = line.split('@')
+//         const [email] = line.split(',');
+//         const myVCard = new VCard()
+//         myVCard
+//             .addName(name+'(magelektronik)')
+//             .addEmail(email)
+
+//         fs.writeFile(`gokcek/${name}(magelektronik).vcf`, myVCard.toString(), function (err) {
+//             if (err) throw err;
+//             console.log('Saved!');
+//         });
+//     }
+//     )
+// }
+// )
+
+// fs.readFile('magrubber.com_users.txt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     const lines = data.split('\n');
+//     lines.forEach(line => {
+//         const [name] = line.split('@')
+//         const [email] = line.split(',');
+//         const myVCard = new VCard()
+//         myVCard
+//             .addName(name+'(magrubber)')
+//             .addEmail(email)
+
+//         fs.writeFile(`gokcek/${name}(magrubber).vcf`, myVCard.toString(), function (err) {
+//             if (err) throw err;
+//             console.log('Saved!');
+//         });
+//     }
+//     )
+// }
+// )
+// fs.readFile('gokcekismakinalari.com_users.txt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     const lines = data.split('\n');
+//     lines.forEach(line => {
+//         const [name] = line.split('@')
+//         const [email] = line.split(',');
+//         const myVCard = new VCard()
+//         myVCard
+//             .addName(name+'(gokcekismakinalari)')
+//             .addEmail(email)
+
+//         fs.writeFile(`gokcek/${name}(gokcekismakinalari).vcf`, myVCard.toString(), function (err) {
+//             if (err) throw err;
+//             console.log('Saved!');
+//         });
+//     }
+//     )
+// }
+// )
+
+
+
+
+
+// const xlsx = require('xlsx');
+// const fs = require('fs');
+
+
+// const workbook = xlsx.readFile('mag-2list.xlsx');
+
+// fs.readFile('bulunamayan.json', 'utf8', function (err, data) {
+
+// })
+
+// 0
+// const sheetName = 'Sheet1';
+
+
+// const worksheet = workbook.Sheets[sheetName];
+
+
+// let rowIndex = 2;
+
+
+// const data = [];
+
+
+// let index = 0;
+// while (true) {
+
+//     const cellB = worksheet['B' + rowIndex];
+
+//     if (!cellB) {
+//         break;
+//     }
+
+//     if (cellB.v === "CATERPILLAR") {
+
+
+//         const cellC = worksheet['D' + rowIndex];
+
+//         if (cellC) {
+
+//             data.push({ partnumber: "" + cellC.v + "" });
+
+//             index++;
+//         }
+//     }
+
+//     rowIndex++;
+// }
+
+
+// fs.writeFileSync('gokcek-caterpillarD.json', JSON.stringify(data), function (err) {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     console.log("Veriler JSON dosyasına yazıldı.");
+// });
+
+
+
+
+
+// obje bolucu
+// const fs = require('fs');
+
+// fs.readFile('gokcek-caterpillar.json', 'utf8', (err, data) => {
+//   if (err) throw err;
+
+//   const objeler = JSON.parse(data);
+
+//   const ilk_1000_obje = objeler.slice(1000, 1901);
+
+//   const yeni_dosya = JSON.stringify(ilk_1000_obje);
+
+//   fs.writeFile('gokcek-caterpillar-kalan901.json', yeni_dosya, (err) => {
+//     if (err) throw err;
+//     console.log('Yeni dosya oluşturuldu!');
+//   });
+// });
+
+
+
+
+
+//karşılaştırma
+// const fs = require('fs');
+
+// const file1 = fs.readFileSync('data2.json');
+// const file2 = fs.readFileSync('gokcek-caterpillar1000.json');
+
+// const data1 = JSON.parse(file1);
+// const data2 = JSON.parse(file2);
+
+// const unmatchedPartNumbers = [];
+
+// data1.forEach((item1) => {
+//   const match = data2.find((item2) => item1.partnumber === item2.partnumber);
+//   if (!match) {
+//     unmatchedPartNumbers.push({partnumber: item1.partnumber});
+//   }
+// });
+
+// data2.forEach((item2) => {
+//   const match = data1.find((item1) => item2.partnumber === item1.partnumber);
+//   if (!match) {
+//     unmatchedPartNumbers.push({partnumber: item2.partnumber});
+//   }
+// });
+
+// const result = {
+//   unmatchedPartNumbers
+// };
+
+// const resultJson = JSON.stringify(result);
+
+// fs.writeFileSync('result.json', resultJson);
+
+
+
+
+//excel b,c ve d kolonlarını alma
+
+const xlsx = require('xlsx');
 const fs = require('fs');
-const VCard = require('vcard-creator').default
 
 
-/* fs.readFile('gokcekmakina.com_users.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    const lines = data.split('\n');
-    lines.forEach(line => {
-        const [name] = line.split('@')
-        const [email] = line.split(',');
-        const myVCard = new VCard()
-        myVCard
-            .addName(name+'(gokcekmakina)')
-            .addEmail(email)
-        fs.mkdir('gokcek', { recursive: true }, (err) => {
-            if (err) throw err;
-        }
-        )
-        fs.writeFile(`gokcek/${name}(gokcekmakina).vcf`, myVCard.toString(), function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-    }
-    )
-}
-) */
-fs.readFile('gokcekgroup.com_users.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    const lines = data.split('\n');
-    lines.forEach(line => {
-        const [name] = line.split('@')
-        const [email] = line.split(',');
-        const myVCard = new VCard()
-        myVCard
-            .addName(name+'(gokcekgroup)')
-            .addEmail(email)
-        
-        fs.writeFile(`gokcek/${name}(gokcekgroup).vcf`, myVCard.toString(), function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-    }
-    )
-}
-)
+const workbook = xlsx.readFile('mag-2list.xlsx');
 
-fs.readFile('magelektronik.com_users.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    const lines = data.split('\n');
-    lines.forEach(line => {
-        const [name] = line.split('@')
-        const [email] = line.split(',');
-        const myVCard = new VCard()
-        myVCard
-            .addName(name+'(magelektronik)')
-            .addEmail(email)
-        
-        fs.writeFile(`gokcek/${name}(magelektronik).vcf`, myVCard.toString(), function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-    }
-    )
-}
-)
 
-fs.readFile('magrubber.com_users.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    const lines = data.split('\n');
-    lines.forEach(line => {
-        const [name] = line.split('@')
-        const [email] = line.split(',');
-        const myVCard = new VCard()
-        myVCard
-            .addName(name+'(magrubber)')
-            .addEmail(email)
-        
-        fs.writeFile(`gokcek/${name}(magrubber).vcf`, myVCard.toString(), function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-    }
-    )
+const worksheet = workbook.Sheets[workbook.SheetNames[1]];
+
+
+const jsonData = [];
+
+
+for (let i = 2; i <= worksheet['!ref'].split(':')[1].substring(1); i++) {
+  const aCell = worksheet['A' + i];
+//   const bCell = worksheet['B' + i];
+//   const cCell = worksheet['C' + i];
+//   const dCell = worksheet['D' + i];
+  
+
+
+
+    const obj = {
+    //   oem: bCell.v,
+      partnumber: ""+aCell.v+"",
+    //   eskiPartNumber: dCell.v
+    };
+
+    jsonData.push(obj);
+  
 }
-)
-fs.readFile('gokcekismakinalari.com_users.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    const lines = data.split('\n');
-    lines.forEach(line => {
-        const [name] = line.split('@')
-        const [email] = line.split(',');
-        const myVCard = new VCard()
-        myVCard
-            .addName(name+'(gokcekismakinalari)')
-            .addEmail(email)
-        
-        fs.writeFile(`gokcek/${name}(gokcekismakinalari).vcf`, myVCard.toString(), function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-    }
-    )
-}
-)
+
+
+fs.writeFile('allpartNumber.json', JSON.stringify(jsonData), function (err) {
+  if (err) throw err;
+  console.log('JSON dosyası oluşturuldu!');
+});
+
+
+
+
+// // eksikleri bulmak için kullanılacak
+// const fs = require('fs');
+
+// // İki JSON dosyasını okuyoruz
+// const dosya1 = fs.readFileSync('caterpillar.json');
+// const dosya2 = fs.readFileSync('bulunamayan.json');
+
+// // JSON verilerini JavaScript nesnelerine dönüştürüyoruz
+// const veri1 = JSON.parse(dosya1);
+// const veri2 = JSON.parse(dosya2);
+
+// const eskiPartNumDizi = [];
+
+// // Her iki dosyanın nesnelerini döngüye alıyoruz
+// for (let i = 0; i < veri1.length; i++) {
+//   for (let j = 0; j < veri2.length; j++) {
+//     // partNumber değerlerini karşılaştırıyoruz
+//     if (veri1[i].partNumber === veri2[j].partNumber) {
+//       // Eşleşen nesnelerin eskiPartNumber değerlerini diziye ekliyoruz
+//       eskiPartNumDizi.push({partnumber:veri1[i].eskiPartNumber});
+//       break; // ikinci dosyada birden fazla aynı partNumber değeri olabilir, bu yüzden ilk eşleşme sonrası döngüden çıkıyoruz
+//     }
+//   }
+// }
+
+// // Diziyi JSON formatına dönüştürüyoruz
+// const yeniVeri = JSON.stringify(eskiPartNumDizi);
+
+// // Yeni dosyaya yazdırıyoruz
+// fs.writeFileSync('caterpillar-eski.json', yeniVeri);
+
+
+
+//sadece partnumberları almak için kullanılacak
+// const fs = require('fs');
+
+
+// const rawData = fs.readFileSync('caterpillar.json');
+// const data = JSON.parse(rawData);
+
+
+// const newData = data.map(item => ({ partnumber:""+ item.partnumber+"" }));
+
+
+// fs.writeFileSync('caterpilla_allpartnumber.json', JSON.stringify(newData));
+
+
+
+
+
+
+
+
+//iki json arasında eşleşmeyen partnumberleri yazdıran kod
+
+// const fs = require('fs');
+
+
+// const rawData1 = fs.readFileSync('caterpillar_allpartnumber.json');
+// const data1 = JSON.parse(rawData1);
+
+
+// const rawData2 = fs.readFileSync('gokcek-caterpillar.json');
+// const data2 = JSON.parse(rawData2);
+
+
+// const newData = [];
+// data1.forEach(item1 => {
+//   const found = data2.some(item2 => item2.partnumber === item1.partnumber);
+//   if (!found) {
+//     newData.push({ partnumber: item1.partnumber });
+//   }
+// });
+
+
+// fs.writeFileSync('eksik_caterpillar.json', JSON.stringify(newData));
+
+
+
+
+
+// const fs = require('fs');
+
+
+// const rawData1 = fs.readFileSync('hazir-caterpillar.json');
+// const data1 = JSON.parse(rawData1);
+
+
+// const rawData2 = fs.readFileSync('mag_guncel.json');
+// const data2 = JSON.parse(rawData2);
+
+
+// const newData = data1.filter(item1 => {
+//   return !data2.some(item2 => item2.partnumber === item1.partnumber);
+// });
+
+
+// fs.writeFileSync('hedef3.json', JSON.stringify(newData));
---
+++ assan/router.js
@@ -0,0 +1,291 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const cheerio = require('cheerio');
+
+const found = undefined;
+var kalanparca =[]
+//18878 ürün kalmış
+//12506 ürün bulmuş
+//32360 ürün varmış
+//976 ürün bulamamış
+//süreyi 15 sn cektım 3 gün kaldı on numara teşekkür eder ve gidiy öptüm
+fs.readFile("./gokcek-caterpillar-50-500.json", "utf8", function (err, datas) {
+  if (err) {
+    console.log(err);
+  }
+  var veri22 = JSON.parse(datas);
+  if (found == undefined) {
+    let ar2 = [];
+
+    veri22.map((keypart, index) => {
+      setTimeout(() => {
+        (async () => {
+          try {
+            const browser =await  puppeteer.launch({
+              headless: false,
+              slowMo: 100,
+            });
+            const page = await browser.newPage();
+            await page.goto("https://avspare.com/");
+            //   await page.select(
+            //     "#ctl00_ctl48_g_e2dc8536_73cb_49a0_9418_8bb9ef07e451_ctl00_ddlSehirler",
+            //     "veri[index].plate.toString()"
+            //   );
+            var tarih = keypart.partnumber;
+            //var tarih = "1P1216";
+            await page.$eval(
+              ".form-control.mr-sm-2.find_all.d-inline-block.w-auto",
+              (e, tarih) => e.setAttribute("value", tarih),
+              tarih
+            );
+
+            await page.click(".btn.btn-success.my-2.my-sm-0");
+            await page.waitForTimeout(2000);
+
+            let element = await page.$(".res_row a");
+            if (element == null) {
+              console.log("---"+keypart.partnumber+"---")
+              kalanparca.push(keypart.partnumber)
+              await page.close();
+              await browser.close();
+            } else {
+              console.log("bulundu" + keypart.partnumber);
+
+              await page.click(".res_row a");
+              await page.waitForTimeout(2000);
+              //col md-9 olanlar
+              let elementColMd9 = await page.$(".col-md-9.col-lg-8.mb-4");
+              if (elementColMd9 == null) {
+                await page.waitForSelector(".col-12.col-lg-11.mb-4");
+
+                let value = await page.$$eval(
+                  ".col-12.col-lg-11.mb-4 h1 span",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                console.log(value)
+                // let value2 = await page.$$eval(
+                //   ".col-12.col-lg-11.mb-4 .mt-4.mb-3",
+                //   (tds) =>
+                //     tds.map((td) => {
+                //       return td.innerText;
+                //     })
+                // );
+
+                let value2 = await page.$$eval(
+                  ".col-12.col-lg-11.mb-4 .mt-4.mb-3",
+                  (tds) =>{
+                        // return td.innerHTML
+                        var arr=[]
+                   
+                        for(var ij=1;ij<tds[0].innerText.split("\n").length-1;ij++){
+                          arr.push(tds[0].innerText.split("\n")[ij])
+                        }
+
+                        return arr 
+                      }
+                );
+                      console.log(value2)
+                let value3 = await page.$$eval(
+                  ".col-12.col-lg-11.mb-4 .bold1",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                    console.log(value3)
+                let value4 = await page.$$eval(
+                  ".col-12.col-lg-11.mb-4 .block_solid",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                var avsParArr=[]
+
+                let elementBaslik = await page.$(".col-12.col-lg-11.mb-4 .card-body .div_show_more");
+                  if(elementBaslik){
+                let baslik1 = await page.$$eval(
+                  ".col-12.col-lg-11.mb-4 .card-body .div_show_more",
+                  (tds) =>{
+                        // return td.innerHTML
+                        var arr=[]
+                       let dss =  tds[0].innerHTML.split("<br>").map((key23,index23)=>{
+                          arr.push(key23)
+                        })
+                        return arr 
+                      }
+                );
+
+                      if(baslik1.length>0){
+                      baslik1.map((keyParca)=>{
+                        const $1 = cheerio.load( keyParca);
+
+                        // Etiketler arasındaki metni çekin
+                        const textBaslik = $1('b').text();
+                        const textMakineAdi = $1('a');
+                       var elementsArray=[];
+                       textMakineAdi.each((i, element33) => {
+                         elementsArray.push($1(element33).text());  // Diziye etiketlerin içeriklerini ekleyin
+                       });
+                        const objPart ={
+                          parca: textBaslik.trim(),
+                          list: elementsArray
+                        }
+                        avsParArr.push(objPart)
+                        
+                      })
+           
+                    }
+
+                  }
+
+                const jsonObject = {
+                  name: value[0].trim(),
+                  code: value2,
+                  netweight: value3.length > 0 ? value3[0] : null,
+                  relatedPart: value4.length > 0 ? value4[0] : null,
+                  partNumber: keypart.partnumber,
+                  compatibledescription: avsParArr.length > 0 ? avsParArr : null,
+                };
+                console.log(jsonObject)
+                fs.readFile("./data4.json", "utf8", function (err, datas) {
+                  var obj = JSON.parse(datas);
+                  obj.push(jsonObject);
+                  jsonStr = JSON.stringify(obj);
+                  fs.writeFile("./data4.json", jsonStr, function (err) {
+                    if (err) throw err;
+                    console.log("Saved!");
+                  });
+                });
+                await page.close();
+                await browser.close();
+              } else {
+                await page.waitForSelector(".col-md-9.col-lg-8.mb-4");
+
+                let value = await page.$$eval(
+                  ".col-md-9.col-lg-8.mb-4 h1 span",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                let value2 = await page.$$eval(
+                  ".col-md-9.col-lg-8.mb-4 .mt-4.mb-3",
+                  (tds) =>{
+                        // return td.innerHTML
+                        var arr=[]
+                   
+                        for(var ij=1;ij<tds[0].innerText.split("\n").length-1;ij++){
+                          arr.push(tds[0].innerText.split("\n")[ij])
+                        }
+
+                        return arr 
+                      }
+                );
+                let value3 = await page.$$eval(
+                  ".col-md-9.col-lg-8.mb-4 .bold1",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                let value4 = await page.$$eval(
+                  ".col-md-9.col-lg-8.mb-4 .block_solid",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                let baslik = await page.$$eval(
+                  ".col-md-9.col-lg-8.mb-4 .card.mb-4 .card-header h2",
+                  (tds) =>
+                    tds.map((td) => {
+                      return td.innerText;
+                    })
+                );
+                // const brContent = await page.evaluate(() => {
+                //   let br = document.querySelector('br');
+                //   return br.innerHTML;
+                // });
+                let baslik142 = await page.$(
+                  ".col-md-9.col-lg-8.mb-4 .card.mb-4 .card-body .div_show_more b"
+                  
+                );
+                // console.log(baslik142)
+
+                let baslik1 = await page.$$eval(
+                  ".col-md-9.col-lg-8.mb-4 .card.mb-4 .card-body .div_show_more",
+                  (tds) =>{
+                        // return td.innerHTML
+                        var arr=[]
+                       let dss =  tds[0].innerHTML.split("<br>").map((key23,index23)=>{
+                          arr.push(key23)
+                        })
+                        return arr 
+                      }
+                );
+                      var avsParArr=[]
+                      baslik1.map((keyParca)=>{
+                        const $1 = cheerio.load( keyParca);
+
+                        // Etiketler arasındaki metni çekin
+                        const textBaslik = $1('b').text();
+                        const textMakineAdi = $1('a');
+                       var elementsArray=[];
+                       textMakineAdi.each((i, element33) => {
+                         elementsArray.push($1(element33).text());  // Diziye etiketlerin içeriklerini ekleyin
+                       });
+                        const objPart ={
+                          parca: textBaslik,
+                          list: elementsArray
+                        }
+                        avsParArr.push(objPart)
+                        
+                      })
+           
+                  
+
+          
+
+                const jsonObject = {
+                  name: value[0],
+                  code: value2,
+                  netweight: value3.length > 0 ? value3[0] : null,
+                  relatedPart: value4.length > 0 ? value4[0] : null,
+                  partNumber: keypart.partnumber,
+                  compatibledescription:avsParArr
+                };
+                fs.readFile("./data4.json", "utf8", function (err, datas) {
+                  var obj = JSON.parse(datas);
+                  obj.push(jsonObject);
+                  jsonStr = JSON.stringify(obj);
+                  fs.writeFile("./data4.json", jsonStr, function (err) {
+                    if (err) throw err;
+                    console.log("Saved!");
+                  });
+                });
+                await page.close();
+                await browser.close();
+              }
+            }
+          } catch (err) {
+            console.log(err)
+          }
+        })();
+      }, 13000 * index);
+    });
+    fs.writeFile('bulunamayan.json',JSON.stringify(kalanparca),(erk,resk)=>{
+      console.log(erk)
+    })
+  } else {
+    console.log("Bu noter zaten kayitli");
+  }
+});
+
+const express = require("express");
+const DutyRouter = express.Router();
+DutyRouter.post("/dutyAll", () => {});
+module.exports = DutyRouter;
